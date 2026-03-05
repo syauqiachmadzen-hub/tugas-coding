cdf8594e753df4559ea2abb164208ac2cf1868a6
syauqiachmadzen <syuqiachmadzen@gmail.com>
2026-03-05 11:42:10 +0700

    commite 4

--- bubbleshort.cpp
+++ bubbleshort.cpp
@@ -25,3 +25,26 @@ void input() {
         cin >> arr{i};
     }
 }
+void bubbleSortArray() {
+    int pass = 1;
+
+    do { 
+        for (int j = 0; j <= n - 1 - pass; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp;
+                temp = arr [j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass passs + 1;
+
+        cout << "\npass" << pass - 1 << ": ";
+        for (int k = 0; k < n; k++) {
+            cout << arr[k] << " ";
+            
+        }
+        cout << endl;
+
+    } while (pass <= n - 1); 
+}
